1c33e02d2c918b9eb4a8375532f44c55039730b3
Evan Devore <eadevore16@gmail.com>
2021-02-01 16:09:48 -0500

    test, don't do this

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -34,7 +34,7 @@ def page_not_found(e):
 def create_app(test_config=None):
     app = Flask(__name__)
     
-    app.config['MONGODB_HOST'] = "mongodb+srv://dbadmin:m4friIWqwX3XkJnU@edevore-cluster.vfigo.mongodb.net/quickVote?retryWrites=true&w=majority"
+    app.config['MONGODB_HOST'] = "mongodb+srv://dbadmin:iH7nQmjieaSB1eG3@edevore-cluster.vfigo.mongodb.net/quickVote?retryWrites=true&w=majority"
     app.config['SECRET_KEY'] = os.urandom(32)
 
     db.init_app(app)
